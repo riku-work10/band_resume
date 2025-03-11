212185c2be34b3a7e76434e23728799ca751d914
riku-work10 <ukir19990530@gmail.com>
2025-03-11 21:41:54 +0900

    deviseとdevise-taken-auth、devise-i18n導入、CORSに追記

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -11,6 +11,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource "*",
       headers: :any,
+      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'], # 認証ヘッダー  reactからリクエストを受け入れられるようにする
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
