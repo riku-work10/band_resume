443a3b2b6ba1beccf2d8697d0c182056fab0550d
riku-work10 <ukir19990530@gmail.com>
2025-03-11 15:49:03 +0900

    テストコード作成、デプロイ用環境変数設定

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] =useState ([]);
+  const API_URL = `${process.env.REACT_APP_API_URL}/tasks`;
+  console.log(API_URL);
+
+  const fetch = async () => {
+    const res = await axios.get(API_URL);
+    setTasks(res.data);
+  };
+
+  useEffect(() => {
+    fetch();
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className="p-6 bg-gray-100 rounded-lg shadow-md">
+  <h1 className="text-3xl text-gray-600 font-semibold text-center mb-4">アプリ開発</h1>
+  <p className="text-lg text-gray-600 mb-6 text-center">テスト！</p>
+  {tasks.map((task, index) => (
+    <div key={index} className="task mb-3 p-4 bg-white rounded-lg shadow-sm flex justify-between items-center">
+      <span className="text-lg text-gray-800">{task.name}</span>
     </div>
-  );
-}
+  ))}
+</div>
+
+  )
+};
 
 export default App;
