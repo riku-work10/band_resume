82e37f44174421b92e092a52ad3c6926fa774fc6
riku-work10 <ukir19990530@gmail.com>
2025-03-12 12:35:22 +0900

    ローカルもVERCEL_URLにした

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['VERCEL_URL'] 
+    origins ENV['VERCEL_URL']
 
     resource "*",
       headers: :any,
