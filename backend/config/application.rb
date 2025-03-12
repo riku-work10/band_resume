e375860b0942d8bb0b7dc3516697d647563015fe
riku-work10 <ukir19990530@gmail.com>
2025-03-12 13:07:59 +0900

    静的URLのみにした

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -52,7 +52,7 @@ module App
       allow do
         # 今回はRailsのポートが3000番、Reactのポートが8000番にするので、Reactのリクエストを許可するためにlocalhost:8000を設定
         # 本番環境も関わってくるから環境変数で設定してあげること！
-        origins ENV['VERCEL_URL']
+        origins 'https://band-resume-git-login-rikus-projects-0196bac5.vercel.app'
         resource '*',
                  :headers => :any,
                  # この一文で、渡される、'access-token'、'uid'、'client'というheaders情報を用いてログイン状態を維持する。
--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['VERCEL_URL']
+    origins 'https://band-resume-git-login-rikus-projects-0196bac5.vercel.app'
 
     resource "*",
       headers: :any,
