0b67730283edac22f1211bbda5ef768d486d504e
riku-work10 <ukir19990530@gmail.com>
2025-03-15 10:32:43 +0900

    履歴書動作確認OK　モデルアソシエーションresume_likesに修正

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   has_many :resumes, dependent: :destroy
   has_many :resume_comments, dependent: :destroy
   has_many :resume_likes
-  has_many :liked_resumes, through: :likes, source: :resume
+  has_many :liked_resumes, through: :resume_likes, source: :resume
 
   validates :email, presence: true, uniqueness: true
 end
