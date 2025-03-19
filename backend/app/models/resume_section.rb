579f18e52c2549c67f5aa00e9c5d71bc365b8562
riku-work10 <ukir19990530@gmail.com>
2025-03-19 21:48:18 +0900

    履歴書セクション、アイテムモデル、アソシエーション、バリデーション作成完了！

--- backend/app/models/resume_section.rb
+++ backend/app/models/resume_section.rb
@@ -1,2 +1,8 @@
 class ResumeSection < ApplicationRecord
+  has_many :resume_items, -> { order(:position) }, dependent: :destroy
+  belongs_to :resume
+
+  validates :position, presence: true, numericality: { only_integer: true }
+  validates :position, uniqueness: { scope: :resume_id }
+
 end
