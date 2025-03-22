359034025a9aac26e9fc6fa4ace083de379a7d91
riku-work10 <ukir19990530@gmail.com>
2025-03-22 16:35:45 +0900

    DNDを縦方向に変更してカードを横幅いっぱいにした

--- frontend/src/components/resumescontents/ResumeSection.jsx
+++ frontend/src/components/resumescontents/ResumeSection.jsx
@@ -19,14 +19,35 @@ export const ResumeSection = ({ index, resumeSectionsList, setResumeSectionsList
   return (
     <Draggable draggableId={resumeSection.id.toString()} index={index}>
       {(provided) => (
-    <div className='taskCard' ref={provided.innerRef} {...provided.draggableProps}>
-      <div className='taskCardTitleAndtaskCardDeleteButtonArea' {...provided.dragHandleProps}>
-      <ResumeSectionTitle  resumeSectionsList={resumeSectionsList} setResumeSectionsList={setResumeSectionsList} resumeSection={resumeSection} resumeId={resumeId}/>
-      <ResumeSectionDeleteButton resumeSectionsList={resumeSectionsList} setResumeSectionsList={setResumeSectionsList} resumeSection={resumeSection} resumeId={resumeId}/>
-      </div>
-      <ResumeItemAddInput inputText={inputText} setInputText={setInputText} itemList={itemList} setItemList={setItemList} resumeSection={resumeSection} resumeId={resumeId}/>
-      <ResumeItems itemList={itemList} setItemList={setItemList} resumeSection={resumeSection} resumeId={resumeId}/>
-    </div>
+        <div
+          className="w-full p-4 bg-white border border-gray-300 rounded-lg shadow-md"
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+        >
+          <div className="flex justify-between items-center mb-4" {...provided.dragHandleProps}>
+            <ResumeSectionTitle
+              resumeSectionsList={resumeSectionsList}
+              setResumeSectionsList={setResumeSectionsList}
+              resumeSection={resumeSection}
+              resumeId={resumeId}
+            />
+            <ResumeSectionDeleteButton
+              resumeSectionsList={resumeSectionsList}
+              setResumeSectionsList={setResumeSectionsList}
+              resumeSection={resumeSection}
+              resumeId={resumeId}
+            />
+          </div>
+          <ResumeItemAddInput
+            inputText={inputText}
+            setInputText={setInputText}
+            itemList={itemList}
+            setItemList={setItemList}
+            resumeSection={resumeSection}
+            resumeId={resumeId}
+          />
+          <ResumeItems itemList={itemList} setItemList={setItemList} resumeSection={resumeSection} resumeId={resumeId} />
+        </div>
       )}
     </Draggable>
-  )};
+  );};
