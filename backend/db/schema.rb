4e402f5905adc8cfea93237ec85afb8c0db8e0af
riku-work10 <ukir19990530@gmail.com>
2025-03-28 14:42:08 +0900

    songsテーブルの作成

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_27_131945) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_28_053909) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -94,6 +94,12 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_27_131945) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_resumes_on_user_id"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tasks", force: :cascade do |t|
