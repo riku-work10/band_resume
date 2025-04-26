7ea115c9e69db3d85ae81042c497206f922d8717
riku-work10 <ukir19990530@gmail.com>
2025-04-26 21:07:12 +0900

    デバッグ確認

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -92,6 +92,6 @@ Rails.application.configure do
   'https://band-resume.onrender.com',
   'https://band-resume.vercel.app'
 ]
-Rails.logger.info "REDIS_URL: #{ENV['REDIS_URL']}"
+puts "REDIS_URL: #{ENV['REDIS_URL']}"
 
 end
