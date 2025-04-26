95aca0de6d041e1439d22d0d5cdb9c2292175758
riku-work10 <ukir19990530@gmail.com>
2025-04-26 16:21:35 +0900

    cable.jsの作成

---
+++ frontend/src/utils/cable.js
@@ -0,0 +1,8 @@
+import { createConsumer } from "@rails/actioncable";
+
+const getCable = (token, uid, client) => {
+  const url = `${process.env.REACT_APP_API_URL}/cable?token=${token}&uid=${uid}&client=${client}`;
+  return createConsumer(url);
+};
+
+export default getCable;
