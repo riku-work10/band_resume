cbbe7e693334f257ba27a40a96428ac07b3324ec
riku-work10 <ukir19990530@gmail.com>
2025-05-01 02:30:55 +0900

    pruductionの修正

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -96,6 +96,7 @@ Rails.application.configure do
     host: 'band-resume.onrender.com',
     protocol: 'https'
   }
+  config.hosts << "band-resume.vercel.app"
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
