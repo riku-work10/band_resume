5520e0f6bbb50f5c733c0e8f54b71d74ca681cd7
riku-work10 <ukir19990530@gmail.com>
2025-05-01 01:38:05 +0900

    perform_deliveries = trueを追加

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -97,6 +97,8 @@ Rails.application.configure do
     protocol: 'https'
   }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
