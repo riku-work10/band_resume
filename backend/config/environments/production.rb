cdaa67beb861435e552770dd8aec05e2fdc4954b
riku-work10 <ukir19990530@gmail.com>
2025-05-01 01:01:57 +0900

    パスワードリセットバック設定

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -92,4 +92,19 @@ Rails.application.configure do
   'https://band-resume.onrender.com',
   'https://band-resume.vercel.app'
 ]
+  config.action_mailer.default_url_options = {
+    host: 'band-resume.onrender.com',
+    protocol: 'https'
+  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
 end
