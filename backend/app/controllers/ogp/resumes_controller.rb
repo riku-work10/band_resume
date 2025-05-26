3e10b39721e858f170d41ac4f547884b63a6e3dd
riku-work10 <ukir19990530@gmail.com>
2025-05-27 01:10:51 +0900

    SHRAEURL変更

--- backend/app/controllers/ogp/resumes_controller.rb
+++ backend/app/controllers/ogp/resumes_controller.rb
@@ -1,4 +1,6 @@
 class Ogp::ResumesController < ApplicationController
+  skip_before_action :authenticate_user! # これが必須
+
   def show
     @resume = Resume.find(params[:id])
     render template: 'ogp/resumes/show', layout: false
