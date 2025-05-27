d8b080b36a3e8d31388ee009d0661584fb16a18d
riku-work10 <ukir19990530@gmail.com>
2025-05-27 12:42:11 +0900

    コントローラに検証ビューを挿入

--- backend/app/controllers/ogp_controller.rb
+++ backend/app/controllers/ogp_controller.rb
@@ -3,6 +3,21 @@ class OgpController < ApplicationController
     @resume = Resume.find_by(id: params[:id])
     return render plain: "Not found", status: 404 unless @resume
 
-    render template: "ogp/resume", layout: false
+    html = <<-HTML
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <meta property="og:title" content="テスト" />
+        <meta property="og:description" content="テストです" />
+        <meta property="og:image" content="https://bandresume.s3.ap-northeast-1.amazonaws.com/profile_images/default_ogp.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </head>
+      <body>
+        <p>動作確認</p>
+      </body>
+      </html>
+    HTML
+
+    render html: html.html_safe, layout: false
   end
 end
