d255baf794dc9657ad6300a640fa6bb1b9d80f91
riku-work10 <ukir19990530@gmail.com>
2025-05-27 17:12:09 +0900

    ogpコントローラをapiモードではなくした

--- backend/app/controllers/ogp_controller.rb
+++ backend/app/controllers/ogp_controller.rb
@@ -1,23 +1,8 @@
-class OgpController < ApplicationController
+class OgpController < ActionController::Base
   def resume
     @resume = Resume.find_by(id: params[:id])
     return render plain: "Not found", status: 404 unless @resume
 
-    html = <<-HTML
-      <!DOCTYPE html>
-      <html>
-      <head>
-        <meta property="og:title" content="テスト" />
-        <meta property="og:description" content="テストです" />
-        <meta property="og:image" content="https://bandresume.s3.ap-northeast-1.amazonaws.com/profile_images/default_ogp.jpg" />
-        <meta name="twitter:card" content="summary_large_image" />
-      </head>
-      <body>
-        <p>動作確認</p>
-      </body>
-      </html>
-    HTML
-
-    render html: html.html_safe, layout: false
+    render template: "ogp/resume", layout: false
   end
 end
