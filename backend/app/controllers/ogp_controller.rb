ccedd7d7c65c38bab00f56d50927a5560967c91c
riku-work10 <ukir19990530@gmail.com>
2025-06-17 02:13:54 +0900

    OGP修正

--- backend/app/controllers/ogp_controller.rb
+++ backend/app/controllers/ogp_controller.rb
@@ -4,14 +4,21 @@ def resume
   return render plain: 'Not found', status: :not_found unless @resume
 
   if bot_request?(request.user_agent)
-    html = render_to_string(template: 'ogp/resume', layout: false)
-    Rails.logger.info "[OGP HTML length] #{html.length}"
-    Rails.logger.info "[OGP HTML snippet] #{html[0..500]}" # 最初の500文字だけ出力
-    render html: html.html_safe
+    Rails.logger.info "@resume: #{@resume.inspect}"
+    begin
+      html = render_to_string(template: 'ogp/resume', layout: false)
+      Rails.logger.info "[OGP HTML length] #{html.length}"
+      Rails.logger.info "[OGP HTML snippet] #{html[0..500]}"
+      render html: html.html_safe
+    rescue => e
+      Rails.logger.error "Render error: #{e.message}"
+      render plain: 'Render error', status: :internal_server_error
+    end
   else
     redirect_to "#{ENV.fetch('VERCEL_URL', nil)}/resumes/#{@resume.id}", allow_other_host: true
   end
 end
+
 
 
   private
