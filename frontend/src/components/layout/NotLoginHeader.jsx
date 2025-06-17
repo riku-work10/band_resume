e52c30df9dee5a1a1ca24df4c63a1ff07113cddf
riku-work10 <ukir19990530@gmail.com>
2025-06-17 12:13:07 +0900

    未ヘッダーにログイン、新規登録追加

--- frontend/src/components/layout/NotLoginHeader.jsx
+++ frontend/src/components/layout/NotLoginHeader.jsx
@@ -27,20 +27,36 @@ function NotLoginHeader() {
 
   return (
     <header className="fixed bg-stone-800 text-white relative">
-      <div className="flex justify-between items-center">
-        {/* ロゴ */}
-        <div className="flex-1 flex justify-start items-center space-x-4">
+<div className="flex items-center h-full">
+        {/* 左：ロゴ */}
+        <div className="flex items-center space-x-4">
           <Link
             to="/"
             className={`text-lg font-bold ${location.pathname === '/' ? 'text-orange-500' : ''}`}
           >
-            <MdHome className="text-xl" />
+            <MdHome className="text-2xl" />
           </Link>
         </div>
-        {/* ハンバーガーメニュー */}
-        <div className="flex-1 flex justify-end items-center space-x-4">
-          <button className="text-white focus:outline-none z-50" onClick={() => setIsOpen(!isOpen)}>
-            {isOpen ? <MdClose className="text-xl" /> : <MdOutlineMenu className="text-xl" />}
+
+        {/* 右側：ログイン・登録・メニュー */}
+        <div className="flex items-center space-x-3 ml-auto">
+          <Link
+            to="/signup"
+            className="px-3 py-1 bg-stone-200 text-stone-800 rounded hover:bg-stone-100 transition"
+          >
+            新規登録
+          </Link>
+          <Link
+            to="/signin"
+            className=" px-3 py-1 bg-stone-600 rounded hover:bg-stone-500 transition"
+          >
+            ログイン
+          </Link>
+          <button
+            className="text-white focus:outline-none z-50"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            {isOpen ? <MdClose className="text-2xl" /> : <MdOutlineMenu className="text-2xl" />}
           </button>
         </div>
       </div>
@@ -52,6 +68,11 @@ function NotLoginHeader() {
           className="menu-container absolute top-12 right-0 w-1/8 bg-stone-700 p-4 shadow-lg rounded-l-lg"
         >
           <ul className="space-y-2">
+            <li>
+              <Link to="/" className="block" onClick={() => setIsOpen(false)}>
+                ホーム
+              </Link>
+            </li>
             <li>
               <Link to="/signin" className="block" onClick={() => setIsOpen(false)}>
                 ログイン
